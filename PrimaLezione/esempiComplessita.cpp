12800ecc9f9be1c2621e88a539d7d97b71432d61
Davide Cazzin <28535750+NeverMendel@users.noreply.github.com>
2021-11-18 19:43:35 +0100

    Remove Big Theta and Big Omega, add template.cpp and esempiComplessita.pptx

--- PrimaLezione/esempiComplessita.cpp
+++ PrimaLezione/esempiComplessita.cpp
@@ -1,5 +1,5 @@
 // Restituire la media di un vettore di interi
-// Complessità Theta(n)
+// Complessità O(n)
 double media(const vector<int>& v) {
     if(v.empty()) return 0;
     int somma = 0;
@@ -10,7 +10,7 @@ double media(const vector<int>& v) {
 }
 
 // Restituire l'indice del primo numero negativo del vettore, -1 se non esiste
-// Complessità O(n) e Omega(1)
+// Complessità O(n)
 int firstNegativeNumberIndex(const vector<int>& v) {
     for (int i = 0; i < v.size(); i++) {
         if (v[i] < 0) return i;
@@ -19,7 +19,7 @@ int firstNegativeNumberIndex(const vector<int>& v) {
 }
 
 // Restituire l'elemento più piccolo in un vettore di interi contentente almeno un elemento
-// Complessità Theta(n)
+// Complessità O(n)
 int minElement(const vector<int>& v) {
     int res = INT_MAX;
     for (int i = 0; i < v.size(); i++) {
@@ -29,13 +29,13 @@ int minElement(const vector<int>& v) {
 }
 
 // Restituire il più piccolo intero in un vettore ordinato in modo non decrescente e contenente almeno un elemento
-// Complessità Theta(1)
+// Complessità O(1)
 int minElementInSortedVector(const vector<int>& v) {
     return v[0];
 }
 
 // Stampare a video il nome dei tre colori RGB
-// Complessità Theta(1)
+// Complessità O(1)
 void printRGBColors(){
     vector<string> colors = {"red", "green", "blue"};
     for(const string& color : colors){
