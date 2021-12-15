b5802d16089b966d3650f43e29cd7d3705505004
Davide Cazzin <28535750+NeverMendel@users.noreply.github.com>
2021-12-15 18:57:56 +0100

    Update G_DogsAndCats.cpp

--- Esercizi/GoogleKickStart/G_DogsAndCats.cpp
+++ Esercizi/GoogleKickStart/G_DogsAndCats.cpp
@@ -1,40 +1,57 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
 #include <string>
+#include <algorithm>
+#include <limits>
 
-#define MAX_SIZE 10000
+using namespace std;
 
-bool solve(long long N, long long D, long long C, const long long & M, const char S[MAX_SIZE]) {
-    for (int i=0; i<N; i++) {
-        if (S[i] == 'C')
-            C--;
-        else if (C<0 || D<=0) {
-            return false;
-        }
-        else {
-            D--;
-            C+=M;
-        }
-    }
-    return true;
+typedef long long ll;
+typedef unsigned int uint;
+
+#define REP(x,l,u) for(ll x = l; x < u; x++)
+#define RREP(x,l,u) for(ll x = l; x >= u; x--)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define mst(x,v) memset(x, v, sizeof(x))
+#define sz(x) (ll)x.size()
+
+void in() {}
+template <typename A> void in(A & x) { cin >> x; }
+template <typename A, typename B> void in(pair<A,B> & x) { in(x.first); in(x.second); }
+template <typename A> void in(vector<A> & x) { REP(i,0,(ll)x.size()) in(x[i]); }
+template <typename Head, typename... Tail> void in(Head & H, Tail & ... T) {in(H); in(T...); }
+
+void solve(){
+	ll n, d, c, m;
+	string s;
+	cin >> n >> d >> c >> m >> s;
+	bool possible = true;
+	for(int i = 0; i < s.size() && possible; i++){
+		if(s[i] == 'C'){
+			c--;
+		} else {
+			if(c < 0) possible = false;
+			d--;
+			if(d < 0) possible = false;
+			c += m;
+		}
+	}
+	cout << (possible ? "YES" : "NO");
 }
 
 int main() {
-    long long T, N, D, C, M;
-    char S[MAX_SIZE+1];
-    std::string sol;
-    std::cin >> T;
-    for(int test=1; test<=T; test++) {
-        std::cin >> N;
-        std::cin >> D;
-        std::cin >> C;
-        std::cin >> M;
-        while(std::cin.peek() != 'C' && std::cin.peek()!='D') {
-            std::cin.ignore();
-        }
-        fgets(S, N+1, stdin);
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
 
-        sol = ((C>=N && D>=N) || solve(N, D, C, M, S))? "YES" : "NO";
-        std::cout << "Case #" << test << ": " << sol << "\n";
-    }
-    return 0;
+	int t;
+	cin >> t;
+	for(int i = 1; i <= t; i++){
+		cout << "Case #" << i << ": ";
+		solve();
+		cout << '\n';
+	}
+
+	return 0;
 }
