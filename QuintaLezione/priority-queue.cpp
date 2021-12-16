17ae88c898405d5c08ef040180757b3cbff79f86
Davide Cazzin <28535750+NeverMendel@users.noreply.github.com>
2021-12-16 09:20:17 +0100

    Change priority-queue.cpp example in QuintaLezione

--- QuintaLezione/priority-queue.cpp
+++ QuintaLezione/priority-queue.cpp
@@ -6,10 +6,15 @@
 
 using namespace std;
 
+bool absValueComparator(const int& a, const int& b){
+  return abs(a) < abs(b);
+}
+
 int main(){
-  priority_queue<int, vector<int>> pq;
+  priority_queue<int, vector<int>, decltype(&absValueComparator)> pq(absValueComparator);
   pq.push(-4);
   pq.push(4);
+  pq.push(-2);
   pq.push(3);
   pq.push(5);
   while(!pq.empty()){
